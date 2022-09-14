440e9971e9e3589f45003a52a1ae07847fdd081c
Victory Ejimofor <victoryejimofor@gmail.com>
2022-09-14 21:14:57 +0100

    Updating the navbar

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('.wrapper-form');
 const alertMsg = document.querySelector('.alertmsg');
-const inputNumber = document.querySelector('.number')
+const inputNumber = document.querySelector('.number');
+const openNavbar = document.querySelector('.navbar');
+const headerList = document.querySelector('.header-navbar')
+const list = document.querySelector('.header-list')
+const logo = document.querySelector('.logo')
+const mobileHeader = document.querySelector('.navbarBtn')
 
 // FORM VALIDATION
 
@@ -33,4 +38,30 @@ function setMessage(errMsg, alertMsg) {
     setTimeout(() => {
     alertMsg.style.display = 'none';
     }, 3000);
+}
+
+// NAV BAR
+
+openNavbar.addEventListener('click', headerDropdown);
+
+// To Open Nav Bar
+function headerDropdown(e) {
+    headerList.style.display = "flex";
+    list.style.display = "block";
+    headerList.style.height = '100vh'
+    headerList.style.backgroundColor = '#252b46';
+    headerList.style.flexDirection = 'column';
+    headerList.style.padding = '50px 40px';
+    list.style.gap = '0px';
+    headerList.style.margin = '0px';
+    openNavbar.style.margin = '0px';
+    logo.style.margin = '0px';
+    logo.style.display = 'none';
+    list.style.flexDirection = 'column';
+    list.style.textAlign = 'center';
+    headerList.style.minWidth = '375px';
+    openNavbar.style.display = 'none';
+    list.style.fontSize = '24px';
+    list.style.fontWeight = '400'
+    mobileHeader.style.padding = '0px'
 }
